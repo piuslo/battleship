8217a7066e562549d0e923aedbe934dbac9b0624
piuslo <piuslo412@gmail.com>
2021-10-01 21:34:29 -0400

    got the game to sort of work for two players

--- battleship.cpp
+++ battleship.cpp
@@ -13,32 +13,69 @@ const int NUM_COLS = 10;
 const int SHIP_LENGTH = 3;
 const int NUM_SHIPS = 4;
 
-
+//Controls the operation of the program.
 int main() {
     srand(time(0));
-    PlayerBoard player;
-    OpponentBoard opponent;
-    for (int i = 0; i < NUM_SHIPS; i++)
-        opponent.assign_ships();
-    int input_x, input_y;
-    int ships_left = NUM_SHIPS * SHIP_LENGTH;
-    // int ships_left2 = NUM_SHIPS * SHIP_LENGTH;
-    while (ships_left != 0) {
-        cout << "Your board:" << endl;
-        player.print();
-        cout << "Opponent's board:" << endl;
-        opponent.print();
-        cout << "Between 0 and 9, input X coordinate: ";
+    PlayerBoard ocean1;
+    OpponentBoard target_2;
+    PlayerBoard ocean2;
+    OpponentBoard target_1;
+    for (int i = 0; i < NUM_SHIPS; i++) {
+        target_2.assign_ships();
+        target_1.assign_ships();
+    }
+    int input_x, input_y = 0;
+    int ships_left1 = NUM_SHIPS;
+    int ships_left2 = NUM_SHIPS;
+    while (ships_left1 != 0 && ships_left2 != 0) {
+        cout << "Player 1's ocean grid" << endl;
+        ocean1.print();
+        cout << "Player 1's targets (Player 2's ships)" << endl;
+        target_2.print();
+        cout << "Player 2's ocean grid" << endl;
+        ocean2.print();
+        cout << "Player 2's targets (Player 1's ships)" << endl;
+        target_1.print();
+        cout << "Player 1's move. Between 0 and 9, input X coordinate:";
         cin >> input_x;
-        cout << "Between 0 and 9, input Y coordinate: ";
+        cout << "Between 0 and 9, input Y coordinate:";
         cin >> input_y;
-        if (opponent.play(input_x, input_y)) {
-            player.hit(input_x, input_y);
-            ships_left--;
+        if (target_2.play(input_x, input_y)) {
+            ocean1.hit(input_x, input_y);
+            ships_left1--;
+            cout << "Hit! Ships left for player 1 to sink: " << ships_left1 << endl;
         } else {
-            player.miss(input_x, input_y);
+            ocean1.miss(input_x, input_y);
+        }
+        if (ships_left1 == 0) {
+            cout << "Game is over. Player 1 has sunk all of Player 2's ships."
+            << endl;
+            return 0;
+        }
+        cout << "Player 1's ocean grid" << endl;
+        ocean1.print();
+        cout << "Player 1's targets (Player 2's ships)" << endl;
+        target_2.print();
+        cout << "Player 2's ocean grid" << endl;
+        ocean2.print();
+        cout << "Player 2's targets (Player 1's ships)" << endl;
+        target_1.print();
+        cout << "Player 2's move. Between 0 and 9, input X coordinate:";
+        cin >> input_x;
+        cout << "Between 0 and 9, input Y coordinate:";
+        cin >> input_y;
+        if (target_1.play(input_x, input_y)) {
+            ocean2.hit(input_x, input_y);
+            ships_left2--;
+            cout << "Hit! Ships left for player 2 to sink: " << ships_left2 << endl;
+        } else {
+            ocean2.miss(input_x, input_y);
+        }
+        if (ships_left2 == 0) {
+            cout << "Game is over. Player 2 has sunk all of Player 1's ships."
+            << endl;
+            return 0;
         }
     }
-    cout << "Success! All ships have been sunk." << endl;
     return 0;
 }
